5e1a1f2db608a84fbc3896ed6b71c8853d12801d
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-22 12:48:10 +0300

    FlightBoardView is creates

---
+++ Flight Shedule/FlightBoardView.swift
@@ -0,0 +1,23 @@
+//
+//  FlightBoardView.swift
+//  Flight Shedule
+//
+//  Created by John Doe on 22/09/2022.
+//
+
+import SwiftUI
+
+struct FlightBoardView: View {
+    let boardName: String
+    
+    var body: some View {
+        Text(boardName)
+            .font(.title)
+    }
+}
+
+struct FlightBoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlightBoardView(boardName: "Arrival")
+    }
+}
